18648f8e3ef49bd19efecbb9a96b6010369e1d67
Mateusz Rokosa <mtrokosa97@gmail.com>
2020-12-02 21:49:46 +0100

    Initial commit

---
+++ src/main/java/com/zrcaw/langshub/LanguageHubApplication.java
@@ -0,0 +1,13 @@
+package com.zrcaw.langshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanguageHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zrcaw/langshub/LanguageHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zrcaw.langshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
