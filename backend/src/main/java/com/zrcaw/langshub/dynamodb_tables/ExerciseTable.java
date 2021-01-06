6e51af90d42020de9af745b0c8294aaf5af7632f
Mateusz Starczyk <238025@student.pwr.edu.pl>
2021-01-06 23:08:12 +0100

    Add options to drop tables and put samples to database

--- backend/src/main/java/com/zrcaw/langshub/dynamodb_tables/ExerciseTable.java
+++ backend/src/main/java/com/zrcaw/langshub/dynamodb_tables/ExerciseTable.java
@@ -1,12 +1,52 @@
 package com.zrcaw.langshub.dynamodb_tables;
 
-public class ExerciseTable extends TablesService{
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.zrcaw.langshub.model.Exercise;
+import com.zrcaw.langshub.service.utils.BeanUtil;
+import org.springframework.util.ResourceUtils;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class ExerciseTable extends TablesService {
+
+    private String tableName = "Exercises";
+    private String samplesFile = "classpath:sample-data/exercises.json";
+    private DynamoDbEnhancedClient enhancedClient = DynamoDbEnhancedClient.builder().dynamoDbClient(ddb).build();
+    private DynamoDbTable<Exercise> exerciseTable = enhancedClient.table("Exercises", TableSchema.fromBean(Exercise.class));
 
     public void createExerciseTable() {
-        String tableName = "Exercises";
-        if(isTableCreated(tableName))
-            return;
+        boolean dropTables = Boolean.parseBoolean(BeanUtil.getProperty("drop.tables"));
+        boolean putSamples = Boolean.parseBoolean(BeanUtil.getProperty("put.samples"));
+        if (isTableCreated(tableName)) {
+            if (dropTables) {
+                deleteTable(tableName);
+                createTable(tableName, "author", "name");
+            }
+        } else
+            createTable(tableName, "author", "name");
+        if (putSamples)
+            putSampleData();
+    }
 
-        createTable(tableName, "author", "name");
+    private void putSampleData() {
+        try {
+            File file = ResourceUtils.getFile(samplesFile);
+            InputStream is = new FileInputStream(file);
+            String json = new String(is.readAllBytes());
+            Type listType = new TypeToken<List<Exercise>>() {
+            }.getType();
+            List<Exercise> exercises = new Gson().fromJson(json, listType);
+            exercises.forEach(exercise -> exerciseTable.putItem(exercise));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
