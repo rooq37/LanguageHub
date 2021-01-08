15ab58b61626e8bb5aa8c9986b71353df5776225
Krzysztof Pilarczyk <radiator0@o2.pl>
2021-01-08 22:56:15 +0100

    Mock login

--- frontend/src/modules/common/navigation/navigation.tsx
+++ frontend/src/modules/common/navigation/navigation.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import { Component } from "react";
 import { Button, Nav, Navbar } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
-import {
-  Link,
-  NavLink,
-  RouteComponentProps,
-  withRouter,
-} from "react-router-dom";
+import { RouteComponentProps, withRouter } from "react-router-dom";
 
 class Navigation extends Component<RouteComponentProps> {
+  logout() {
+    localStorage.clear();
+  }
+
   render() {
+    const loggedInUser = localStorage.getItem("user");
     return (
       <Navbar bg="primary" expand="lg" className="navbar-dark">
         <Navbar.Brand href="/">LanguageHub</Navbar.Brand>
@@ -27,9 +27,23 @@ class Navigation extends Component<RouteComponentProps> {
               <Nav.Link>About</Nav.Link>
             </LinkContainer>
           </Nav>
-          <LinkContainer to="/login">
-            <Button variant="outline-secondary">Login</Button>
-          </LinkContainer>
+          {loggedInUser ? (
+            <React.Fragment>
+              <b className="text-light mr-3">Hello {loggedInUser}! </b>
+              <LinkContainer to="/login">
+                <Button
+                  variant="outline-secondary"
+                  onClick={this.logout.bind(this)}
+                >
+                  Logout
+                </Button>
+              </LinkContainer>
+            </React.Fragment>
+          ) : (
+            <LinkContainer to="/login">
+              <Button variant="outline-secondary">Login</Button>
+            </LinkContainer>
+          )}
         </Navbar.Collapse>
       </Navbar>
     );
--- frontend/src/modules/login/login.tsx
+++ frontend/src/modules/login/login.tsx
@@ -1,9 +1,65 @@
 import React from "react";
 import { Component } from "react";
+import { Form, Button, Col, Row, Alert } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
-class Login extends Component {
+export interface ITranslatorStates {
+  usernameSelect: string;
+}
+
+class Login extends Component<{}, ITranslatorStates> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      usernameSelect: "Meffiu",
+    };
+  }
+
+  login() {
+    localStorage.setItem("user", this.state.usernameSelect);
+  }
+
+  handleUserChange(event) {
+    this.setState({ usernameSelect: event.target.value });
+  }
+
   render() {
-    return <p>Login</p>;
+    const loggedInUser = localStorage.getItem("user");
+    return loggedInUser ? (
+      <Alert variant="warning">Please logout before login!</Alert>
+    ) : (
+      <Form>
+        <p className="text-center">
+          <b>Please select your user from predefined:</b>
+          <br />
+          Note: all roles (lecturer as well ;)) are disabled now.
+        </p>
+        <Row>
+          <Col sm={3}></Col>
+          <Col>
+            <Form.Group>
+              <Form.Control
+                as="select"
+                defaultValue={this.state.usernameSelect}
+                onChange={this.handleUserChange.bind(this)}
+              >
+                <option value="Meffiu">Meffiu (lecturer)</option>
+                <option value="Andrzej">Andrzej (lecturer)</option>
+                <option value="Stachu">Stachu (lecturer)</option>
+                <option value="Tomo">Tomo (lecturer)</option>
+              </Form.Control>
+            </Form.Group>
+          </Col>
+          <Col>
+            <LinkContainer to="/">
+              <Button variant="primary" onClick={this.login.bind(this)}>
+                Login
+              </Button>
+            </LinkContainer>
+          </Col>
+        </Row>
+      </Form>
+    );
   }
 }
 
