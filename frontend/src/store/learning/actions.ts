13e0690784db5b8ccdc3f954f78288061528fd05
Krzysztof Pilarczyk <radiator0@o2.pl>
2021-01-11 21:31:20 +0100

    Add loadingbar to lessons

--- frontend/src/store/learning/actions.ts
+++ frontend/src/store/learning/actions.ts
@@ -2,23 +2,29 @@ import { ACTION_TYPES } from './types';
 import axios from 'axios';
 import { InfoResponse } from '../../dtos/info-response';
 import { ISolution } from '../../models/learning/solution.model';
+import { showLoading, hideLoading } from 'react-redux-loading-bar';
 
 export function createSolution(solution: ISolution) {
   return function(dispatch){
+    dispatch(showLoading())
     return axios.post<InfoResponse>(`http://localhost:8080/api/learning`,  solution )
     .then(json => {
       dispatch({type: ACTION_TYPES.CREATE_SOLUTION, payload: json});
+      dispatch(hideLoading());
     }).catch(error => {
       dispatch({type: ACTION_TYPES.CREATE_SOLUTION, payload: error.response});
+      dispatch(hideLoading());
   })
   };
 }
 
 export function getPupilExercises(pupilName: string) {
   return function(dispatch){
+    dispatch(showLoading())
     return axios.get(`http://localhost:8080/api/learning?pupilName=` + pupilName)
     .then(json => {
       dispatch({type: ACTION_TYPES.GET_PUPIL_EXERCISES, payload: json});
+      dispatch(hideLoading());
     })
   };
 }
