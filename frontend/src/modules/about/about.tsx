9fedb6f2095996908626f90e0b37ee4fb6386a71
Krzysztof Pilarczyk <radiator0@o2.pl>
2021-01-12 10:22:39 +0100

    Add loremipsum

--- frontend/src/modules/about/about.tsx
+++ frontend/src/modules/about/about.tsx
@@ -1,9 +1,111 @@
 import React from "react";
 import { Component } from "react";
+import { CardGroup, Card } from "react-bootstrap";
 
 class About extends Component {
   render() {
-    return <p>ABOUT</p>;
+    return (
+      <React.Fragment>
+        <CardGroup>
+          <Card style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>Lorem ispum 1</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Lorem ispum 1
+              </Card.Subtitle>
+              <Card.Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                quis congue mauris. Proin condimentum tempus dolor, et gravida
+                arcu. Sed mauris lacus, finibus at fringilla sit amet,
+                consectetur in elit. Suspendisse volutpat nisi vel nibh
+                consectetur, a lacinia dui iaculis. Duis semper ligula faucibus
+                augue pharetra mattis.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>Lorem ispum 2</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Lorem ispum 2
+              </Card.Subtitle>
+              <Card.Text>
+                Morbi lacinia purus nec nibh semper semper. Integer dignissim
+                venenatis ex, id rutrum massa. Integer nulla arcu, ullamcorper
+                vitae venenatis viverra, porta vitae ligula. Etiam tempor nunc
+                eget erat accumsan ornare. Integer id dolor vestibulum, posuere
+                mauris non, egestas leo. In commodo leo non enim scelerisque,
+                pharetra posuere elit ullamcorper.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>Lorem ispum 3</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Lorem ispum 3
+              </Card.Subtitle>
+              <Card.Text>
+                Quisque ullamcorper eu eros dictum pulvinar. Fusce at gravida
+                risus. Phasellus vel condimentum quam. Nam viverra urna tortor,
+                luctus congue erat eleifend pharetra. Aenean mollis nibh sed
+                egestas fermentum. Non fermentum sapien. Vestibulum mi odio,
+                hendrerit eu nisi at, euismod maximus odio.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </CardGroup>
+        <CardGroup>
+          <Card style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>Lorem ispum 4</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Lorem ispum 4
+              </Card.Subtitle>
+              <Card.Text>
+                Quisque ullamcorper eu eros dictum pulvinar. Fusce at gravida
+                risus. Phasellus vel condimentum quam. Nam viverra urna tortor,
+                luctus congue erat eleifend pharetra. Aenean mollis nibh sed
+                egestas fermentum. Non fermentum sapien. Vestibulum mi odio,
+                hendrerit eu nisi at, euismod maximus odio.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>Lorem ispum 5</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Lorem ispum 5
+              </Card.Subtitle>
+              <Card.Text>
+                Morbi lacinia purus nec nibh semper semper. Integer dignissim
+                venenatis ex, id rutrum massa. Integer nulla arcu, ullamcorper
+                vitae venenatis viverra, porta vitae ligula. Etiam tempor nunc
+                eget erat accumsan ornare. Integer id dolor vestibulum, posuere
+                mauris non, egestas leo. In commodo leo non enim scelerisque,
+                pharetra posuere elit ullamcorper.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>Lorem ispum 6</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Lorem ispum 6
+              </Card.Subtitle>
+              <Card.Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                quis congue mauris. Proin condimentum tempus dolor, et gravida
+                arcu. Sed mauris lacus, finibus at fringilla sit amet,
+                consectetur in elit. Suspendisse volutpat nisi vel nibh
+                consectetur, a lacinia dui iaculis. Duis semper ligula faucibus
+                augue pharetra mattis.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </CardGroup>
+      </React.Fragment>
+    );
   }
 }
 
